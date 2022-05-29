14a36bf846fe0837eb9160e7ceec8fc7d3b2f0a6
yz <791018061@qq.com>
2022-05-29 21:30:53 +0800

    module01, task 1.2

--- module01/main/1.1.go
+++ module01/main/1.1.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/*
+给定一个字符串数组
+[“I”,“am”,“stupid”,“and”,“weak”]
+用 for 循环遍历该数组并修改为
+[“I”,“am”,“smart”,“and”,“strong”]
+*/
 func main() {
 	var src = [5]string{"I", "am", "stupid", "and", "weak"}
 
---
+++ module01/main/1.2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+/*
+基于 Channel 编写一个简单的单线程生产者消费者模型：
+
+- 队列：
+    队列长度 10，队列元素类型为 int
+- 生产者：
+    每 1 秒往队列中放入一个类型为 int 的元素，队列满时生产者可以阻塞
+- 消费者：
+    每一秒从队列中获取一个元素并打印，队列为空时消费者阻塞
+
+*/
+func main() {
+	ch := make(chan int, 10)
+
+	go func(ch chan<- int) {
+		rand.Seed(time.Now().UnixNano())
+		timer := time.NewTicker(time.Second)
+		for {
+			t := <-timer.C
+			next := rand.Intn(10)
+			fmt.Println(t.Format("2006-01-02 15:04:05"), "sending:", next)
+			ch <- next
+		}
+	}(ch)
+
+	for v := range ch {
+		fmt.Println("receiving:", v)
+	}
+
+}
